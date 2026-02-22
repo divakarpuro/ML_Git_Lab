64affcf70e1cf7799573e5964c27efe35eaf0405
Divakar Purohit <divakarpurohit@Divakars-MacBook-Air-3.local>
2026-02-22 23:30:08 +0530

    adding model

---
+++ model.py
@@ -0,0 +1,26 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score
+
+data = pd.read_csv("Housing.csv")
+X = data.drop("price", axis=1)
+y = data["price"]
+
+X = pd.get_dummies(X, drop_first=True)
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+
+model = LinearRegression()
+model.fit(X_train, y_train)
+
+
+y_pred = model.predict(X_test)
+
+r2 = r2_score(y_test, y_pred)
+
+print("Model Trained Successfully!")
+print("R2 Score:", r2)
