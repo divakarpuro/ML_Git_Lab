c046cc6342e169cee74594894f0d147212ed6097
Divakar Purohit <divakarpurohit@Divakars-MacBook-Air-3.local>
2026-02-22 23:41:26 +0530

    adding scaler

--- model.py
+++ model.py
@@ -14,6 +14,10 @@ X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
 
+scaler = StandardScaler()  
+X_train = scaler.fit_transform(X_train)  
+X_test = scaler.transform(X_test)
+
 model = LinearRegression()
 model.fit(X_train, y_train)
 
